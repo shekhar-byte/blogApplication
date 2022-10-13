4b2d19cfd56cd2abb9159d33efc1d65ca4832c9f
shekhar <shekharmishra.sage@gmail.com>
2022-10-13 21:44:58 +0530

    added static file

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -17,16 +17,6 @@ export default function Write() {
       desc,
       photo
     };
-    // if (file) {
-    //   const data =new FormData();
-    //   const filename = Date.now() + file.name;
-    //   data.append("name", filename);
-    //   data.append("file", file);
-    //   newPost.photo = filename;
-    //   try {
-    //     await axios.post("/upload", data);
-    //   } catch (err) {}
-    // }
     try {
       const res = await axios.post("/posts", newPost);
       window.location.replace("/post/" + res.data._id);
